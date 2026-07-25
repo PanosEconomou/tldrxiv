cecf503cd94ae4af0a6014e53d3684bb5e961723
Panos Economou <panos.economou.v@gmail.com>
2026-07-24 20:14:34 -0400

    reading toml config file

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ A lot of open source guides were used in this so here are some credits!
  - [Python Packaging Guide](https://packaging.python.org/en/latest/tutorials/packaging-projects/)
  - [The Hitchiker's Guide to Python/Packaging](https://docs.python-guide.org/writing/structure/)
  - [Regex Patterns in Python](https://docs.python.org/3/howto/regex.html)
+ - [Reading TOML in Python](https://realpython.com/python-toml/)
+
---
+++ src/tldrxiv/config.py
@@ -0,0 +1,64 @@
+# ------------------------- #
+#  ┓ ┓  ┏┓┏┓•               #
+# ╋┃┏┫┏┓ ┃┃ ┓┓┏             #
+# ┗┗┗┻┛ ┗┛┗┛┗┗┛             #
+#                           # 
+# Reading the config        #
+# ------------------------- #
+
+import os
+import tomllib
+from pathlib import Path
+
+_DEFAULTS = {
+    "arxiv"     : {
+            "feeds" : ["hep-th"],
+            "type"  : ["new", "cross"],
+        },
+
+    "storage"   : {
+        "daily_arxiv"   : 1,
+        "daily_digest"  : 60,
+        },
+
+    "llm"       : {
+        "api_key"       : os.environ.get("TLDRXIV_LLM_KEY", ""),
+        "temperature"   : 0.3,
+        "url"           : "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent",
+        },
+
+    "research"  : {
+        "work"                  : "Generalized symmetry in particular through the lens of 2D CFTs. Interest in Conformal Defects and boundaries as a probe for studying RG flows as well as the mathematical structure of generalized symmetry.",
+        "research_interests"    : "Generalized Symmetry as a method for understanding both the mathematical structure of QFT as well as obtaining new kinematical results in specific contenxts. Also some interest in lattices and condensed matter applications of generalized symmetry. Always on the lookout for where generalized symmetry ideas but not necessarily techniques or formalism can be applied in novel areas."
+        },
+}
+
+def _find_config_path(cli_path: Path | None) -> Path | None:
+    if cli_path is not None: 
+        if cli_path.is_file(): 
+            return cli_path
+    return None
+
+def _parse_config_file(cfg:dict, path:Path) -> dict:
+    try:
+        with open(path, "rb") as file:
+            local_cfg = tomllib.load(file)
+        cfg.update(local_cfg)
+    except tomllib.TOMLDecodeError as error:
+        print(f"Failed to parse config file {path}. Using Default config.")
+        print(error)
+    except OSError as error:
+        print(f"Cannot read config file {path}. Using Default config.")
+    return cfg
+
+def load(cli:dict) -> dict:
+    """
+    Parses the config file and overrides any arguments from the cli
+    """
+
+    cfg     = _DEFAULTS.copy()
+    path    = _find_config_path(cli["config_file"])
+    if path is not None: cfg = _parse_config_file(cfg, path)
+    cfg.update(cli)
+        
+    return cfg
